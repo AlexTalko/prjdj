5aa0337d6e74d7e03d1374e78ee5997c75356df4
air <talko.alexander@gmail.com>
2024-05-27 13:41:48 +0400

    make migrate

--- catalog/models.py
+++ catalog/models.py
@@ -22,6 +22,8 @@ class Product(models.Model):
         auto_now=True, verbose_name="Дата последнего изменения"
     )
 
+    # manufactured_at = models.DateTimeField(auto_now=True, verbose_name="Дата производства продукта")
+
     def __str__(self):
         return f"{self.name}, {self.description}, {self.price}"
 
