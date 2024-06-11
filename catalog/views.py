16a0f8f172abd5720c7263d3c4d6de07ec25ff5b
air <talko.alexander@gmail.com>
2024-06-11 13:31:40 +0400

    upd views

--- catalog/views.py
+++ catalog/views.py
@@ -1,53 +1,24 @@
 from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView, DetailView, TemplateView
 from catalog.models import Category, Product
 
 
-def contacts(request):
-    if request.method == "POST":
-        name = request.POST.get("name")
-        phone = request.POST.get("phone")
-        message = request.POST.get("message")
-        print(f'{name} {phone}\n{message}')
-    context = {'title': 'Контакты'}
-    return render(request, 'catalog/contacts.html', context)
+class ContactsTemplateView(TemplateView):
+    template_name = 'catalog/contacts.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Контакты'
+        return context
 
 
-def category_main_page(request, ):
-    category = Category.objects.all()
-    context = {
-        'category_list': category,
-        'title': 'Каталог товаров',
-    }
-    return render(request, 'catalog/category_main_page.html', context)
+class CategoryListView(ListView):
+    model = Category
 
 
-def category_one(request, pk):
-    category_one = get_object_or_404(Category, pk=pk)
-    prod_cat = category_one.products.all()
-
-    context = {
-        'category_one': category_one,
-        'title': f'{category_one.name}',
-        'prod_cat': prod_cat,
-    }
-    return render(request, 'catalog/category_one.html', context)
+class CategoryDetailView(DetailView):
+    model = Category
 
 
-def products(request):
-    product = Product.objects.all()
-    context = {
-        'product_list': product,
-        'title': 'Товары',
-    }
-    return render(request, 'catalog/products.html', context)
-
-
-def product_detail(request, pk):
-    product_one = get_object_or_404(Product, pk=pk)
-
-    context = {
-        'product_one': product_one,
-        'title': f'{product_one.name}',
-
-    }
-    return render(request, 'catalog/prod_info.html', context)
+class ProductDetailView(DetailView):
+    model = Product
