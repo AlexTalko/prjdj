34450fecd21210cc4f6d4e3f9f04e23a08bb888d
air <talko.alexander@gmail.com>
2024-06-11 14:14:09 +0400

    add app blog

--- config/settings.py
+++ config/settings.py
@@ -29,7 +29,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'catalog'
+    'catalog',
+    'blog',
 ]
 
 MIDDLEWARE = [
@@ -50,12 +51,16 @@ TEMPLATES = [
         'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
+
             'context_processors': [
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
+            'builtins': [
+                'catalog.templates.templatetags.my_tags',
+            ]
         },
     },
 ]
