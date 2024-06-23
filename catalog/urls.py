1afff491014f4df3acf769a851d58eff40d70224
air <talko.alexander@gmail.com>
2024-06-23 21:43:25 +0400

    добавлена возможность добавить, редактировать и удалить товар

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from catalog.views import CategoryListView, CategoryDetailView, ProductDetailView, ContactsTemplateView
+from catalog.views import CategoryListView, CategoryDetailView, ProductDetailView, ContactsTemplateView, \
+    ProductCreateView, ProductUpdateView, ProductDeleteView
 from catalog.apps import CatalogConfig
 
 app_name = CatalogConfig.name
@@ -8,5 +9,8 @@ urlpatterns = [
     path('contacts/', ContactsTemplateView.as_view(), name='contacts'),
     path('', CategoryListView.as_view(), name='category_list'),
     path('catalog/<int:pk>/', CategoryDetailView.as_view(), name='category_detail'),
-    path('product_detail/<int:pk>/', ProductDetailView.as_view(), name='product_detail')
+    path('product_detail/<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
+    path('create/', ProductCreateView.as_view(), name='product_create'),
+    path('update/<int:pk>', ProductUpdateView.as_view(), name='product_update'),
+    path('delete/<int:pk>', ProductDeleteView.as_view(), name='product_delete'),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404
-from django.urls import reverse
-from django.views.generic import ListView, DetailView, TemplateView
+from django.urls import reverse, reverse_lazy
+from django.views.generic import ListView, DetailView, TemplateView, CreateView, UpdateView, DeleteView
+
+from catalog.forms import ProductForm
 from catalog.models import Category, Product
 
 
@@ -26,3 +28,25 @@ class CategoryDetailView(DetailView):
 
 class ProductDetailView(DetailView):
     model = Product
+    form_class = ProductForm
+    success_url = reverse_lazy('catalog:category_detail')
+
+
+class ProductCreateView(CreateView):
+    model = Product
+    fields = ('name', 'description', 'image', 'price', )
+    success_url = reverse_lazy('catalog:category_list')
+
+
+class ProductUpdateView(UpdateView):
+    model = Product
+    form_class = ProductForm
+    success_url = reverse_lazy('catalog:category_list')
+
+    def get_success_url(self):
+        return reverse('catalog:product_detail', args=[self.kwargs.get('pk')])
+
+
+class ProductDeleteView(DeleteView):
+    model = Product
+    success_url = reverse_lazy('catalog:category_list')
